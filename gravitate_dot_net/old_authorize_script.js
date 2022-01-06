0010c96e4d6e13e31269d114fbfc0683edd9f874
Matt Rothmeyer <25441636+TenTonsOfHat@users.noreply.github.com>
2022-01-06 15:47:04 -0600

    Small reorg

--- gravitate_dot_net/old_authorize_script.js
+++ gravitate_dot_net/old_authorize_script.js
@@ -1,77 +1,20 @@
-var authServiceUrl = getFilledUrl('authService');
-var validationService = getFilledUrl('validationService');
-
-console.log(authServiceUrl)
-console.log(validationService)
-
-var username = getVariable('username');
-var password = getVariable('password');
-var alwaysRefresh = getVariable('alwaysRefresh');
-
 var sdk = require('postman-collection');
-console.log(validationService)
-var isValidTokenRequest = new sdk.Request({
-    url: validationService, // Use an endpoint that requires being authenticated
-    method: 'POST',
-    header: [
-        new sdk.Header({
-            key: 'content-type',
-            value: 'application/json',
-        }),
-        new sdk.Header({
-            key: 'acccept',
-            value: 'application/json',
-        }),
-        new sdk.Header({
-            key: 'Authorization',
-            value: 'Bearer ' + pm.collectionVariables.get("jwttoken"),
-        }),
-    ]
-});
+var authServiceUrl = BuildURL('authService');
+console.log(`Auth Service URL: ${authServiceUrl}`)
 
+var alwaysRefresh = GetFromEnvironmentOrCollection('alwaysRefresh');
 if(alwaysRefresh == 'true'){
-    refreshToken();
+    RefreshToken();
 }else {
-    pm.sendRequest(isValidTokenRequest, function (err, response) {
-        if (response.code === 401) {
-            refreshToken();
-        }
-    });
+    ValidateAndRefreshToken();
 } 
 
-function getFilledUrl(paramName){
-    var result = pm.environment.replaceIn(
-        pm.collectionVariables.replaceIn(pm.collectionVariables.get(paramName))
-    );
-    return result;
-}
-
-
-function getVariable(variableName, required){
-
-    var environment = pm.environment.get(variableName)
-    var collection = pm.collectionVariables.get(variableName)
-    
-    var useEnvironment = environment != undefined && environment != null
-    var collectionValid = collection != undefined || collection != null
-
-    var result = undefined;
-    if(useEnvironment) {
-        result = environment;
-        console.log(`Pulled ${variableName} from environment, value ${result}`)
-    }else if(collectionValid){
-        result = collection;
-        console.log(`Pulled ${variableName} from collectionVariables, value ${result}`)
-    }else if (required !== false) {
-        throw `Could not find a value for ${variableName} in environment or collectionVariables`
-    }
-
-    return result;
-}
-
-function refreshToken() {
+function RefreshToken() {
+    var username = GetFromEnvironmentOrCollection('username');
+    var password = GetFromEnvironmentOrCollection('password');
+    var url = `${authServiceUrl}?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
     var tokenRequest = new sdk.Request({
-    url: authServiceUrl+"?username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password),
+    url: url,
     method: 'POST',
     header: [
         new sdk.Header({
@@ -102,11 +45,65 @@ function refreshToken() {
       else {
           throw new Error(`Could not log in, details below: ${JSON.stringify(payload, 0, 2)}`);
       }
-
-        
-
-      
   }); 
+}
 
-  
+function ValidateAndRefreshToken(){
+    console.log(validationService)
+    var validationService = BuildURL('validationService');
+    console.log(validationService)
+    var isValidTokenRequest = new sdk.Request({
+        url: validationService, // Use an endpoint that requires being authenticated
+        method: 'POST',
+        header: [
+            new sdk.Header({
+                key: 'content-type',
+                value: 'application/json',
+            }),
+            new sdk.Header({
+                key: 'acccept',
+                value: 'application/json',
+            }),
+            new sdk.Header({
+                key: 'Authorization',
+                value: 'Bearer ' + pm.collectionVariables.get("jwttoken"),
+            }),
+        ]
+    });
+
+    pm.sendRequest(isValidTokenRequest, function (err, response) {
+        if (response.code === 401) {
+            RefreshToken();
+        }
+    });
+}
+
+// Gets a variable value from the environment or collection
+function GetFromEnvironmentOrCollection(variableName, required){
+
+    var environment = pm.environment.get(variableName)
+    var collection = pm.collectionVariables.get(variableName)
+    
+    var useEnvironment = environment != undefined && environment != null
+    var collectionValid = collection != undefined || collection != null
+
+    var result = undefined;
+    if(useEnvironment) {
+        result = environment;
+        console.log(`Pulled ${variableName} from environment, value ${result}`)
+    }else if(collectionValid){
+        result = collection;
+        console.log(`Pulled ${variableName} from collectionVariables, value ${result}`)
+    }else if (required !== false) {
+        throw `Could not find a value for ${variableName} in environment or collectionVariables`
+    }
+
+    return result;
+}
+// Fills a URL value from the environment with a value from the collection
+function BuildURL(paramName){
+    var result = pm.environment.replaceIn(
+        pm.collectionVariables.replaceIn(pm.collectionVariables.get(paramName))
+    );
+    return result;
 }
