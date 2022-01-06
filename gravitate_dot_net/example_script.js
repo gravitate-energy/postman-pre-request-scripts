752e3c05a64a0ab84a3f0ec18adbd041864d209f
Matt Rothmeyer <25441636+TenTonsOfHat@users.noreply.github.com>
2022-01-06 16:53:40 -0600

    Updates to example pre request script

--- gravitate_dot_net/example_script.js
+++ gravitate_dot_net/example_script.js
@@ -1,12 +1,24 @@
-var url = "https://raw.githubusercontent.com/gravitate-energy/postman-pre-request-scripts/"
-var scriptPath = `${url}master/gravitate_dot_net/old_authorize_script.js`
+EvaluateGithubScript(
+    "https://raw.githubusercontent.com/gravitate-energy/postman-pre-request-scripts/master/gravitate_dot_net",
+    "old_authorize_script.js",
+    true
+)
 
-
-pm.sendRequest(scriptPath, function (err, res) {
+function EvaluateGithubScript(baseUrl, scriptName, logResponse){
+    var uuid = require('uuid');
+    // adding token and a random string causes github raw to return the latest result and bypass its cache
+    var cacheBuster = `token=${uuid.v4()}` 
+    var requestUrl = `${baseUrl}/${scriptName}?${cacheBuster}`
+    console.log(`Script URL: ${requestUrl}`)
+    pm.sendRequest(requestUrl, function (err, res) {
     if (err) {
         console.log(err);
     } else {
         var response = res.text();
+        if(logResponse){
+            console.log(response)
+        }
         eval(response)
     }
 });
+}
